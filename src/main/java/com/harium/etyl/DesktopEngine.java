a2c1d67cc77d0cef987df22d73c45a6cf5715f88
Yuri Pourre <yuripourre@gmail.com>
2017-12-06 22:57:40 -0200

    Initial structure

---
+++ src/main/java/com/harium/etyl/DesktopEngine.java
@@ -0,0 +1,24 @@
+package com.harium.etyl;
+
+public abstract class DesktopEngine {
+
+    protected int w;
+    protected int h;
+
+    public DesktopEngine(int w, int h) {
+        super();
+
+        this.w = w;
+        this.h = h;
+    }
+
+    public abstract void init();
+
+    public abstract void setTitle(String title);
+
+    public abstract void setIcon(String icon);
+
+    public abstract void enableFullScreen();
+
+    public abstract void disableFullScreen();
+}
